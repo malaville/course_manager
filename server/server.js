1263122611437894e6ef02d9742944172a428f8e
Marc-Antoine Laville <marc-antoine.laville.c@thalesdigital.io>
2019-02-13 11:28:47 +0100

    Setup Production build and server

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+const publicPath = path.join(__dirname, "../", "public");
+const port = process.env.PORT || 3000;
+//Tell him to serve public/
+app.use(express.static(publicPath));
+//MiddleWare, code that runs when someone makes a request
+app.get("*", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
+});
+app.listen(port, () => {
+  console.log("server is up !");
+});
